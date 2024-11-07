ba83df69d0508f0b6137e271461df57785a37e65
Alexei Peters <apeters@fargeo.com>
2024-11-07 14:22:19 -0800

    stubbing out linked section component, re #30

--- arches_provenance/src/arches_provenance/EditableReport/EditableReport.vue
+++ arches_provenance/src/arches_provenance/EditableReport/EditableReport.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 console.log("HELLO WORLD!");
+import LinkedSection from './components/LinkedSection.vue'; 
+
 </script>
 
 <template>
     <div>
         <h1>HELLO WORLD!</h1>
+        <LinkedSection />
     </div>
 </template>
---
+++ arches_provenance/src/arches_provenance/EditableReport/components/LinkedSection.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import Button from 'primevue/button';
+
+
+let config = {
+    title: "Linked Section",
+    linked_sections: [
+        {   
+            id: "1",
+            label: "Section 1",
+        },{
+            id: "2",
+            label: "Section 2",
+        },
+    ]
+};
+
+</script>
+
+<template>
+
+    <div>
+        <Button v-for="linked_section in config.linked_sections" :key="linked_section.id" :label="linked_section.label" :href="'#'+linked_section.id" />
+        <a v-for="linked_section in config.linked_sections" :key="linked_section.id" :href="'#'+linked_section.id" >{{ linked_section.label }}</a>
+    </div>
+
+    <div>
+        <section style="height:700px;" :id="linked_section.id" v-for="linked_section in config.linked_sections" :key="linked_section.id">
+            <h2>{{ linked_section.label }}</h2>
+        </section>
+    </div>
+
+</template>
+
+<style scoped>
+    html {
+        scroll-behavior: smooth;
+    }
+</style>
