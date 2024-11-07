95fa21239bcc4d734b04107de91ac83dc669aff5
Jacob Walls <jwalls@fargeo.com>
2024-11-07 16:59:24 -0500

    Stub out default report config #27

--- arches_provenance/models.py
+++ arches_provenance/models.py
@@ -5,9 +5,19 @@ from arches.app.models.models import GraphModel
 
 class ReportConfig(models.Model):
     id = models.AutoField(primary_key=True)
-    config = models.JSONField()
-    graph = models.ForeignKey(GraphModel, on_delete=models.CASCADE) 
+    config = models.JSONField(blank=True, null=False, default=dict)
+    graph = models.ForeignKey(GraphModel, blank=False, on_delete=models.CASCADE)
 
     class Meta:
         managed = True
         db_table = "arches_provenance_report_config"
+
+    def clean(self):
+        if not self.config:
+            self.config = self.generate_config()
+
+    def generate_config(self):
+        return {
+            str(card.name): []
+            for card in self.graph.cardmodel_set.order_by("sortorder")
+        }
