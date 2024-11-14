8422b6344fcfacb68cc3d967ff0b6a3b1d7f90a5
Jacob Walls <jwalls@fargeo.com>
2024-11-14 13:57:13 -0500

    Render a component from a config #46

--- arches_provenance/src/arches_provenance/EditableReport/EditableReport.vue
+++ arches_provenance/src/arches_provenance/EditableReport/EditableReport.vue
@@ -1,18 +1,64 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
-import { fetchReportConfig } from "@/arches_provenance/EditableReport/api.ts";
+import { defineAsyncComponent, onMounted, ref } from "vue";
+import { useGettext } from "vue3-gettext";
 
+import { useToast } from "primevue/usetoast";
+
+import { fetchReportConfig } from "@/arches_provenance/EditableReport/api.ts";
+import { DEFAULT_ERROR_TOAST_LIFE } from "@/arches_provenance/constants.ts";
+
+import type {
+    NamedSection,
+    SectionContent,
+} from "@/arches_provenance/EditableReport/types";
+
+const toast = useToast();
+const { $gettext } = useGettext();
 const resourceId = window.location.href.split("/").reverse()[0];
-const config = ref({ name: "Loading..." });
+const componentLookup = {};
+
+const config: NamedSection = ref({
+    name: "Loading...",
+    content: [{ component: "", config: {} }],
+});
 
 onMounted(async () => {
-    config.value = await fetchReportConfig(resourceId);
+    try {
+        config.value = await fetchReportConfig(resourceId);
+    } catch (error) {
+        toast.add({
+            severity: "error",
+            life: DEFAULT_ERROR_TOAST_LIFE,
+            summary: $gettext("Unable to fetch report config"),
+            detail: error.message,
+        });
+    }
+    config.value.content.forEach((content: SectionContent) => {
+        componentLookup[content.component] = defineAsyncComponent(
+            () =>
+                import(
+                    `@/arches_provenance/EditableReport/sections/${content.component}.vue`
+                ),
+        );
+    });
 });
 </script>
 
 <template>
-    <div style="display: flex; flex-direction: row">
-        <pre style="width: 33%; text-wrap: auto">{{ config }}</pre>
-        <h1>{{ config.name }}</h1>
+    <div class="section-container">
+        <h2>{{ config.name }}</h2>
+        <!--Consider <keep-alive> if future refactors cause these to be rerendered.-->
+        <component
+            :is="componentLookup[content.component]"
+            v-for="content in config.content"
+            :key="content.component"
+            :content
+        />
     </div>
 </template>
+
+<style scoped>
+.section-container {
+    gap: 2rem;
+}
+</style>
