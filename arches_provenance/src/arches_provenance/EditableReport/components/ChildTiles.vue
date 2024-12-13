bf032cb8162dfd8a89f3d34283c94e5c1d02fd42
Jacob Walls <jwalls@fargeo.com>
2024-12-13 10:39:30 -0500

    Improve fetching

--- arches_provenance/src/arches_provenance/EditableReport/components/ChildTiles.vue
+++ arches_provenance/src/arches_provenance/EditableReport/components/ChildTiles.vue
@@ -1,11 +1,40 @@
 <script setup lang="ts">
-import { fetchChildTileData } from "@/arches_provenance/EditableReport/api.ts";
+import { onMounted, ref } from "vue";
+import { useGettext } from "vue3-gettext";
 
+import { useToast } from "primevue/usetoast";
+
+import { fetchChildTileData } from "@/arches_provenance/EditableReport/api.ts";
+import { DEFAULT_ERROR_TOAST_LIFE } from "@/arches_provenance/constants.ts";
 import ChildTile from "@/arches_provenance/EditableReport/components/ChildTile.vue";
+
+import type { LabelBasedTile } from "@/arches_provenance/EditableReport/types";
 
 const props = defineProps<{ tileId: string }>();
 
-const childTileData = await fetchChildTileData(props.tileId);
+const toast = useToast();
+const { $gettext } = useGettext();
+
+const isLoading = ref(true);
+const childTileData = ref<LabelBasedTile[]>([]);
+
+async function fetchData() {
+    try {
+        childTileData.value = await fetchChildTileData(props.tileId);
+    } catch (error) {
+        toast.add({
+            severity: "error",
+            life: DEFAULT_ERROR_TOAST_LIFE,
+            summary: $gettext("Unable to fetch resource"),
+            detail: error instanceof Error ? error.message : String(error),
+        });
+        throw error;
+    } finally {
+        isLoading.value = false;
+    }
+}
+
+onMounted(fetchData);
 </script>
 
 <template>
@@ -19,9 +48,9 @@ const childTileData = await fetchChildTileData(props.tileId);
         />
     </template>
     <p
-        v-if="!childTileData.length"
+        v-if="!isLoading && !childTileData.length"
         style="margin: 0 4.25rem"
     >
-        {{ $gettext("No further data found.") }}
+        {{ $gettext("No further data found") }}
     </p>
 </template>
