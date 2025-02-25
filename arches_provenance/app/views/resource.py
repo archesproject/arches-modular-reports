9668f4f2b1b18c183e49df48aebdc1620ab662e7
vouidaskis <mpampis.vouidaskis@gmail.com>
2025-02-25 17:50:28 +0200

    delete the extra library

--- arches_provenance/app/views/resource.py
+++ arches_provenance/app/views/resource.py
@@ -16,54 +16,22 @@ You should have received a copy of the GNU Affero General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
-import json
 import logging
-import uuid
-from django.db import connection
-
-from django.core.exceptions import ObjectDoesNotExist, PermissionDenied
-from django.contrib.auth.models import User, Group
-from django.forms.models import model_to_dict
-from django.http import HttpResponseNotFound
 from django.http import Http404
-from django.http import HttpResponseBadRequest, JsonResponse
 from django.shortcuts import render
-from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.utils.translation import gettext as _
-from django.views.generic import View
-from django.utils import translation
 
 from arches.app.models import models
-from arches.app.models.card import Card
+
 from arches.app.models.graph import Graph
-from arches.app.models.tile import Tile
-from arches.app.models.resource import Resource, PublishedModelError
-from arches.app.models.system_settings import settings
-from arches.app.utils.activity_stream_jsonld import ActivityStreamCollection
-from arches.app.utils.betterJSONSerializer import JSONSerializer, JSONDeserializer
-from arches.app.utils.decorators import group_required
-from arches.app.utils.decorators import can_edit_resource_instance
+
+from arches.app.models.resource import Resource
 from arches.app.utils.decorators import can_read_resource_instance
-from arches.app.utils.i18n import LanguageSynchronizer, localize_complex_input
-from arches.app.utils.pagination import get_paginator
-from arches.app.utils.permission_backend import (
-    get_default_settable_permissions,
-    user_is_resource_editor,
-    user_is_resource_reviewer,
-    user_can_delete_resource,
-)
-from arches.app.utils.response import JSONResponse, JSONErrorResponse
-from arches.app.utils.string_utils import str_to_bool
-from arches.app.search.search_engine_factory import SearchEngineFactory
-from arches.app.search.mappings import RESOURCES_INDEX
-from arches.app.views.base import BaseManagerView, MapBaseManagerView
-from arches.app.views.concept import Concept
-from arches.app.datatypes.datatypes import DataTypeFactory
+
+from arches.app.views.base import MapBaseManagerView
 
 from arches.app.models.models import ResourceInstance
-
-import arches.app.utils.permission_backend as apb
 
 logger = logging.getLogger(__name__)
 
