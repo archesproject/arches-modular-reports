7a3544fc1b47b531227ac99cad6ca3e2f98a5e34
Alexei Peters <apeters@fargeo.com>
2025-03-04 19:01:58 -0800

    remove references to old group report, re #212

---
+++ arches_provenance/migrations/212_remove_old_group_report.py
@@ -0,0 +1,18 @@
+from django.db import migrations
+from arches.app.models.models import ReportTemplate
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("arches_provenance", "11500_change_default_standard_search"),
+    ]
+
+    def forwards_func(apps, schema_editor):
+        ReportTemplate.objects.delete(componentname="provenance_group_report")
+
+    def reverse_func(apps, schema_editor):
+        pass
+
+    operations = [
+        migrations.RunPython(forwards_func, reverse_func),
+    ]
