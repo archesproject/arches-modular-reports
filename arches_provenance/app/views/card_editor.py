4c2e0d0a297c81c9cee0ec34d478723dd9c134a5
Jacob Walls <jwalls@fargeo.com>
2025-04-10 16:13:32 -0400

    Remove tile edit API access (#276)

--- arches_provenance/app/views/card_editor.py
+++ arches_provenance/app/views/card_editor.py
@@ -1,4 +1,8 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
 
 from arches_querysets.rest_framework.permissions import Guest
 from arches_querysets.rest_framework.serializers import (
@@ -8,16 +12,16 @@ from arches_querysets.rest_framework.serializers import (
 from arches_querysets.rest_framework.view_mixins import ArchesModelAPIMixin
 
 
-class ProvenanceResourceDetailView(ArchesModelAPIMixin, RetrieveUpdateDestroyAPIView):
-    permission_classes = [Guest]
-    serializer_class = ArchesResourceSerializer
+# class ProvenanceResourceDetailView(ArchesModelAPIMixin, RetrieveUpdateDestroyAPIView):
+#     permission_classes = [Guest]
+#     serializer_class = ArchesResourceSerializer
 
 
-class ProvenanceTileListCreateView(ArchesModelAPIMixin, ListCreateAPIView):
-    permission_classes = [Guest]
-    serializer_class = ArchesTileSerializer
+# class ProvenanceTileListCreateView(ArchesModelAPIMixin, ListCreateAPIView):
+#     permission_classes = [Guest]
+#     serializer_class = ArchesTileSerializer
 
 
-class ProvenanceTileDetailView(ArchesModelAPIMixin, RetrieveUpdateDestroyAPIView):
+class ProvenanceTileDetailView(ArchesModelAPIMixin, RetrieveAPIView):
     permission_classes = [Guest]
     serializer_class = ArchesTileSerializer
