6938f703da56b5d5bf5d8478abafb13c6c0049e9
Alexei Peters <apeters@fargeo.com>
2025-04-18 12:24:02 -0700

    add endpoint, re #284

--- arches_provenance/urls.py
+++ arches_provenance/urls.py
@@ -22,6 +22,7 @@ from arches_provenance.app.views.editable_report import (
     NodeTileDataView,
     ProvenanceEditableReportConfigView,
     RelatedResourceView,
+    UserPermissionsView,
 )
 from arches_provenance.app.views.related_resource import ProvenanceRelatedResourcesView
 
@@ -254,6 +255,11 @@ urlpatterns = [
         "api/provenance_tile/<slug:nodegroup_alias>/<uuid:pk>",
         ProvenanceTileDetailView.as_view(),
         name="api_provenance_tile",
+    ),
+    path(
+        "api/user_permissions",
+        UserPermissionsView.as_view(),
+        name="api_user_permissions",
     ),
 ]
 
