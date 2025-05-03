d13ffd1907f634e234dcef76de0fcf783ff38818
Jacob Walls <jwalls@fargeo.com>
2025-05-03 07:19:21 +0100

    Remove distinct() from relations query

--- arches_provenance/app/utils/nodegroup_tile_data_utils.py
+++ arches_provenance/app/utils/nodegroup_tile_data_utils.py
@@ -370,7 +370,6 @@ def get_sorted_filtered_relations(
                 nodeid__nodegroup_id__in=permitted_nodegroups,
             )
         )
-        .distinct()
         .annotate(
             relation_name_json=(
                 models.CardXNodeXWidget.objects.filter(node=OuterRef("nodeid")).values(
