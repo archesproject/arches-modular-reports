938b6d0446127737030e78d569bd8d1f61a12de5
Jacob Walls <jwalls@fargeo.com>
2025-06-02 15:47:32 -0400

    Clean up fallback provision

--- arches_provenance/app/views/editable_report.py
+++ arches_provenance/app/views/editable_report.py
@@ -230,6 +230,11 @@ class NodePresentationView(APIBase):
             )
         )
 
+        def getattr_from_queryset(queryset, attr, fallback):
+            if queryset:
+                return getattr(queryset[0], attr, fallback)
+            return fallback
+
         def get_node_visibility(node):
             if node.pk == node.nodegroup.pk and node.nodegroup.cardmodel_set.all():
                 return node.nodegroup.cardmodel_set.all()[0].visible
@@ -238,7 +243,6 @@ class NodePresentationView(APIBase):
             return True
 
         return JSONResponse(
-            # Coerce nulls to simplify front-end handling.
             {
                 node.alias: {
                     "nodeid": node.nodeid,
@@ -246,25 +250,25 @@ class NodePresentationView(APIBase):
                     # TODO(jtw): consider removing datatype once all display
                     # values calculated by resource serializer
                     "datatype": node.datatype,
-                    "card_name": (
-                        node.nodegroup.cardmodel_set.all()[0].name or ""
-                        if node.nodegroup.cardmodel_set.all()
-                        else ""
+                    "card_name": getattr_from_queryset(
+                        node.nodegroup.cardmodel_set.all(),
+                        "name",
+                        "",
                     ),
-                    "card_order": (
-                        node.nodegroup.cardmodel_set.all()[0].sortorder or 0
-                        if node.nodegroup.cardmodel_set.all()
-                        else 0
+                    "card_order": getattr_from_queryset(
+                        node.nodegroup.cardmodel_set.all(),
+                        "sortorder",
+                        0,
                     ),
-                    "widget_label": (
-                        node.cardxnodexwidget_set.all()[0].label
-                        if node.cardxnodexwidget_set.all()
-                        else node.name.replace("_", " ").title()
+                    "widget_label": getattr_from_queryset(
+                        node.cardxnodexwidget_set.all(),
+                        "label",
+                        node.name.replace("_", " ").title(),
                     ),
-                    "widget_order": (
-                        node.cardxnodexwidget_set.all()[0].sortorder or 0
-                        if node.cardxnodexwidget_set.all()
-                        else 0
+                    "widget_order": getattr_from_queryset(
+                        node.cardxnodexwidget_set.all(),
+                        "sortorder",
+                        0,
                     ),
                     "visible": get_node_visibility(node),
                     "nodegroup": {
