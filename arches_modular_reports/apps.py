5dab2b3025da8bb7349b79c16a5e074d31a1fcd8
Christopher Byrd <chrabyrd@gmail.com>
2025-06-17 15:31:08 -0700

    fixes modular reports to run as a standalone app

--- arches_modular_reports/apps.py
+++ arches_modular_reports/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
-
+from arches import VERSION as arches_version
+from arches.settings_utils import generate_frontend_configuration
 
 class ArchesModularReportsConfig(AppConfig):
     name = "arches_modular_reports"
     is_arches_application = True
+
+    def ready(self):
+        from arches.app.models.system_settings import settings
+
+        if arches_version < (8, 0):
+            if settings.APP_NAME.lower() == self.name:
+                generate_frontend_configuration()
