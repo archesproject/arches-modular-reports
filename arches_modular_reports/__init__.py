38dd3267b8d9cff810720835b3d84a0fc556bce9
Alexei Peters <apeters@fargeo.com>
2025-06-30 15:49:26 -0700

    updates to package dependencies

--- arches_modular_reports/__init__.py
+++ arches_modular_reports/__init__.py
@@ -1,7 +1,3 @@
-from __future__ import absolute_import
-from arches.version import get_version
+import importlib.metadata
 
-# VERSION[3] options = "alpha", "beta", "rc", or "final"
-VERSION = (1, 0, 0, "beta", 0)
-
-__version__ = get_version(VERSION)
+__version__ = importlib.metadata.version(__package__)
