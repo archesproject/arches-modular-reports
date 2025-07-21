ace513540caacbbf4547cdb28a3651fe0862daa7
Christopher Byrd <chrabyrd@gmail.com>
2025-07-21 11:34:17 -0700

    refreshes report on save

--- arches_modular_reports/src/arches_modular_reports/ModularReport/components/ResourceEditor/components/CardEditor/CardEditor.vue
+++ arches_modular_reports/src/arches_modular_reports/ModularReport/components/ResourceEditor/components/CardEditor/CardEditor.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
-import { inject } from "vue";
+import { inject, useTemplateRef } from "vue";
+
+import Button from "primevue/button";
 
 import DefaultCard from "@/arches_component_lab/cards/DefaultCard/DefaultCard.vue";
 
@@ -8,25 +10,47 @@ import { EDIT } from "@/arches_component_lab/widgets/constants.ts";
 const graphSlug = inject<string>("graphSlug");
 const resourceInstanceId = inject<string>("resourceInstanceId");
 
+const defaultCard = useTemplateRef("defaultCard");
+
 const { selectedNodegroupAlias } = inject("selectedNodegroupAlias") as {
     selectedNodegroupAlias: string | null;
 };
 const { selectedTileId } = inject("selectedTileId") as {
     selectedTileId: string | null | undefined;
 };
+
+const emit = defineEmits(["save"]);
+
+async function save() {
+    if (defaultCard.value) {
+        await defaultCard.value.save();
+    }
+}
 </script>
 
 <template>
     <DefaultCard
         v-if="selectedNodegroupAlias && graphSlug"
+        ref="defaultCard"
         :mode="EDIT"
         :nodegroup-alias="selectedNodegroupAlias"
         :graph-slug="graphSlug"
         :resource-instance-id="resourceInstanceId"
+        :should-show-form-buttons="false"
         :tile-id="selectedTileId"
+        @save="
+            console.log('save', $event);
+            emit('save', $event);
+        "
         @update:widget-dirty-states="
             console.log('update:widgetDirtyStates', $event)
         "
         @update:tile-data="console.log('update:tileData', $event)"
+    />
+
+    <Button
+        class="p-button-secondary"
+        label="Save"
+        @click="save"
     />
 </template>
