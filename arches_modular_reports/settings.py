ceb2725edecb8b593bb67880befa11a91347b20b
Cyrus Hiatt <chiatt@fargeo.com>
2025-07-28 09:59:24 -0700

    Move arches include into a url file for use when modular_reports is run as project

--- arches_modular_reports/settings.py
+++ arches_modular_reports/settings.py
@@ -58,7 +58,7 @@ SECRET_KEY = "django-insecure-bw-qkksf4f&^a9o5ykl511-3ym$_d*2e9&s!jv6e%%j9g24)7f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ROOT_URLCONF = "arches_modular_reports.urls"
+ROOT_URLCONF = "arches_modular_reports.urls_local"
 ROOT_HOSTCONF = "arches_modular_reports.hosts"
 
 DEFAULT_HOST = "arches_modular_reports"
