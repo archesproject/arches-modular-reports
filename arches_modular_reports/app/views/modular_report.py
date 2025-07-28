5be869a77780432a68a6945a3105c61bd2f66add
Aaron Gundel <aaron.gundel@gmail.com>
2025-07-28 13:40:43 -0600

    pr feedback

--- arches_modular_reports/app/views/modular_report.py
+++ arches_modular_reports/app/views/modular_report.py
@@ -42,7 +42,9 @@ from arches_modular_reports.app.utils.nodegroup_tile_data_utils import (
 @method_decorator(can_read_resource_instance, name="dispatch")
 class ModularReportConfigView(View):
     def get(self, request):
-        filters = Q(graph__resourceinstance=request.GET.get("resourceId"))
+        filters = Q(graph__source_identifier=None) & Q(
+            graph__resourceinstance=request.GET.get("resourceId")
+        )
 
         report = request.GET.get("reportConfigName", None)
         if report:
