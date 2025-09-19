76d3b46865f3e59ac3ef9f1fab6d102f7aa625f3
Alexei Peters <apeters@fargeo.com>
2025-09-19 13:20:07 -0700

    fix rendering of rich text in datatable, re #110

--- arches_modular_reports/src/arches_modular_reports/ModularReport/components/DataSection.vue
+++ arches_modular_reports/src/arches_modular_reports/ModularReport/components/DataSection.vue
@@ -107,11 +107,12 @@ const columnData = computed(() => {
     return props.component.config.node_aliases.map((nodeAlias) => {
         const nodeDetails = nodePresentationLookup.value![nodeAlias];
         return {
-            nodeAlias,
+            nodeAlias: nodeAlias,
             widgetLabel:
                 props.component.config.custom_labels?.[nodeAlias] ??
                 nodeDetails?.widget_label ??
                 nodeAlias,
+            is_rich_text: nodeDetails?.is_rich_text,
         };
     });
 });
@@ -353,6 +354,9 @@ function initiateEdit(tileId: string | null) {
                         v-else-if="data[field as string]?.is_file"
                         :file-data="data[field as string].file_data"
                     />
+                    <template v-else-if="data.is_rich_text">
+                        <span v-html="data[field as string]?.display_value"></span>
+                    </template>
                     <template v-else>
                         {{ data[field as string]?.display_value }}
                     </template>
