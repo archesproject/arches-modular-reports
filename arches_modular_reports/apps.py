217d588e3bde05657cf565cccdb65bbfe8125ba6
Aaron Gundel <124614+aarongundel@users.noreply.github.com>
2026-02-27 13:20:52 -0700

    Adds config generators

--- arches_modular_reports/apps.py
+++ arches_modular_reports/apps.py
@@ -5,3 +5,10 @@ class ArchesModularReportsConfig(AppConfig):
     name = "arches_modular_reports"
     verbose_name = "Arches Modular Reports"
     is_arches_application = True
+
+    def ready(self):
+        from django.db.models.signals import post_save
+        from arches.app.models.models import GraphModel
+        from arches_modular_reports.signals import handle_graph_post_save
+
+        post_save.connect(handle_graph_post_save, sender=GraphModel)
