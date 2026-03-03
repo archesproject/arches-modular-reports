e625856974c282e017d51256abcd4b70cdfe117b
Aaron Gundel <124614+aarongundel@users.noreply.github.com>
2026-03-03 16:27:02 -0700

    nit

--- arches_modular_reports/apps.py
+++ arches_modular_reports/apps.py
@@ -1,4 +1,9 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_save
+from arches.app.models.models import GraphModel
+from arches_modular_reports.signals import handle_graph_post_save
+from arches_modular_reports.config_generators import register
+from arches_modular_reports.models import ReportConfig
 
 
 class ArchesModularReportsConfig(AppConfig):
@@ -7,12 +12,6 @@ class ArchesModularReportsConfig(AppConfig):
     is_arches_application = True
 
     def ready(self):
-        from django.db.models.signals import post_save
-        from arches.app.models.models import GraphModel
-        from arches_modular_reports.signals import handle_graph_post_save
-        from arches_modular_reports.config_generators import register
-        from arches_modular_reports.models import ReportConfig
-
         post_save.connect(handle_graph_post_save, sender=GraphModel)
 
         def _default_factory(graph):
