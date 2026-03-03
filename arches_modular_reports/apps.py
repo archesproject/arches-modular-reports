b3903ffe2f075e585fd918e3970c065502035cfc
Aaron Gundel <124614+aarongundel@users.noreply.github.com>
2026-03-03 16:00:10 -0700

    adds a default generator for modular reports

--- arches_modular_reports/apps.py
+++ arches_modular_reports/apps.py
@@ -10,5 +10,13 @@ class ArchesModularReportsConfig(AppConfig):
         from django.db.models.signals import post_save
         from arches.app.models.models import GraphModel
         from arches_modular_reports.signals import handle_graph_post_save
+        from arches_modular_reports.config_generators import register
+        from arches_modular_reports.models import ReportConfig
 
         post_save.connect(handle_graph_post_save, sender=GraphModel)
+
+        def _default_factory(graph):
+            rc = ReportConfig(graph=graph)
+            return rc.generate_config()
+
+        register("default", _default_factory)
