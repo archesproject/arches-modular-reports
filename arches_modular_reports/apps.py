36275a66907087536dac4be1ab6250b864065bb5
Christopher Byrd <chrabyrd@gmail.com>
2026-07-20 13:01:41 -0700

    nit

--- arches_modular_reports/apps.py
+++ arches_modular_reports/apps.py
@@ -1,4 +1,7 @@
 from django.apps import AppConfig
+from django.conf import settings
+
+from arches.settings_utils import generate_frontend_configuration
 
 
 class ArchesModularReportsConfig(AppConfig):
@@ -7,6 +10,9 @@ class ArchesModularReportsConfig(AppConfig):
     is_arches_application = True
 
     def ready(self):
+        if settings.APP_NAME.lower() == self.name:
+            generate_frontend_configuration()
+
         # Imports are deferred to ready() to avoid AppRegistryNotReady errors.
         # Django models cannot be imported at module level in AppConfig subclasses.
         from arches_modular_reports.config_generator_registry import register
